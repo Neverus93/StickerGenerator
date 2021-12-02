28b5440c075caa8b7a5a674f3b1114e8a83d1edb
Neverus93 <svsilent2054@gmail.com>
2021-12-03 00:17:49 +0300

    Minor structure changes. Minor refactoring/

---
+++ StickerGenerator_DocX/Model/StickerInfo.cs
@@ -0,0 +1,21 @@
+﻿namespace StickerGenerator_DocX.Model
+{
+    public class StickerInfo
+    {
+        public string Article { get; set; }
+        public string ArticleCRM { get; set; }
+        public string ChipName { get; set; }
+
+        public StickerInfo()
+        {
+
+        }
+
+        public StickerInfo(string article, string articleCRM, string chipName)
+        {
+            Article = article;
+            ArticleCRM = articleCRM;
+            ChipName = chipName;
+        }
+    }
+}
---
+++ StickerGenerator_DocX/ViewModel/StickerViewModel.cs
@@ -0,0 +1,42 @@
+﻿using StickerGenerator_DocX.Command;
+using StickerGenerator_DocX.Model;
+using System;
+using System.Windows;
+
+namespace StickerGenerator_DocX.ViewModel
+{
+    public class StickerViewModel
+    {
+        public int CurrentNumber { get; set; }
+        public RelayCommand RelayCommand { get; }
+        public StickerInfo Sticker { get; set; }
+        public string FileName { get; set; }
+        public int CountOfBoxes { get; set; }
+
+        public StickerViewModel()
+        {
+            RelayCommand = new RelayCommand(Click);
+            Sticker = new StickerInfo();
+        }
+
+        private void Click(object parameter)
+        {
+            try
+            {
+                if (CountOfBoxes > 0 && CurrentNumber > 0)
+                {
+                    StickersCreator.CreateStickers(FileName, Sticker, CountOfBoxes, CurrentNumber);
+                    MessageBox.Show($"Готово\nДокумент \"{FileName}\" успешно сохранён!");
+                }
+                else
+                {
+                    throw new InvalidOperationException("Что-то пошло не так. Проверьте введённые данные...");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error, MessageBoxResult.OK);
+            }
+        }
+    }
+}
