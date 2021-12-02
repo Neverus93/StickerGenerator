6ab3e2d336d41fcd0d3d76980a381679b423f386
Neverus93 <svsilent2054@gmail.com>
2021-12-03 00:48:41 +0300

    Добавлен проект, в котором будет Unit-тестирование

---
+++ StickerGeneratorTests/StickersCreatorTests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace StickerGeneratorTests
+{
+    public class StickersCreatorTests
+    {
+        [Test]
+        public void CreateStickersTest()
+        {
+            Assert.Pass();
+        }
+    }
+}
